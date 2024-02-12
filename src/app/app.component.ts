17afe5316fbe4c0ba82a7e7a3c1c6cbcd8ab1e61
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-02-12 18:21:08 +0100

    initial commit

---
+++ src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.less']
+})
+export class AppComponent {
+  title = '2023-24-2-angular';
+}
