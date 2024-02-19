97c8b01f7d697569b36e2356def6389054eb3a35
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-02-19 18:27:03 +0100

    Gyak 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NzCardModule } from 'ng-zorro-antd/card';
+
+const zorroModules = [
+  NzCardModule
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
+    ...zorroModules,
     BrowserModule,
     AppRoutingModule
   ],
