f3fba4c0a05c19638245a395153d1685f1c64c80
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-02-19 19:10:37 +0100

    Gyak 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,16 @@ import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NotFoundComponent } from './not-found/not-found.component';
+import {NzFormModule} from "ng-zorro-antd/form";
+import {NzButtonModule} from "ng-zorro-antd/button";
+import {NzInputModule} from "ng-zorro-antd/input";
 
 const zorroModules = [
-  NzCardModule
+  NzButtonModule,
+  NzCardModule,
+  NzFormModule,
+  NzInputModule
+
 ]
 
 @NgModule({
