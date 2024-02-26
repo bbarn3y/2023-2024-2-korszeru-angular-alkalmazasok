ec5450ee3a52e6bd1c6a1c2ce8b2a497bb07d0e9
barnabas.balazs <barnabas.balazs@enliven.systems>
2024-02-26 19:02:18 +0100

    Gyak 3

--- src/app/_services/client.service.ts
+++ src/app/_services/client.service.ts
@@ -1,16 +1,21 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {map, Observable} from "rxjs";
+import {UserService} from "./user.service";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientService {
 
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient,
+              private userService: UserService) { }
 
   login(): Observable<{ token: string }> {
-    return this.http.get<{ token: string }>('https://mocki.io/v1/4b986d28-e3c8-4b39-90ca-80cec8d7d01d');
+    return this.http.get<{ token: string }>('https://mocki.io/v1/4b986d28-e3c8-4b39-90ca-80cec8d7d01d')
+      .pipe(map((response) => {
+        this.userService.saveSession(response.token);
+        return response;
+      }))
   }
-
 }
--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -4,6 +4,23 @@ import { Injectable } from '@angular/core';
   providedIn: 'root'
 })
 export class UserService {
+  isLoggedIn: boolean;
+  sessionKey = 'session';
 
-  constructor() { }
+  constructor(
+  ) {
+    this.isLoggedIn = !!localStorage.getItem(this.sessionKey);
+  }
+
+  removeSession(): void {
+    this.isLoggedIn = false;
+    localStorage.removeItem(this.sessionKey);
+  }
+
+  saveSession(token: string): void {
+    this.isLoggedIn = true;
+    localStorage.setItem(this.sessionKey, token);
+  }
+
+
 }
